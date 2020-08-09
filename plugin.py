f6d49fb6325b23080043b6b1dbe05bbcec512af0
Ihor Oleksandrov <ihodev@gmail.com>
2020-08-09 18:52:29 +0300

    Fix: Correctly resolve ng probe location

--- plugin.py
+++ plugin.py
@@ -15,8 +15,8 @@ class LspAngularPlugin(NpmClientHandler):
     package_name = __package__
     server_directory = "server"
     server_binary_path = os.path.join(server_directory, "node_modules", "@angular", "language-server", "index.js")
-    ng_probe_location = os.path.join(sublime.cache_path(), package_name, server_directory, "node_modules")
 
     @classmethod
     def get_binary_arguments(cls):
-        return ['--ngProbeLocations', cls.ng_probe_location, '--tsProbeLocations', cls.ng_probe_location, '--stdio']
+        ng_probe_location = os.path.join(sublime.cache_path(), cls.package_name, cls.server_directory, "node_modules")
+        return ['--ngProbeLocations', ng_probe_location, '--tsProbeLocations', ng_probe_location, '--stdio']
