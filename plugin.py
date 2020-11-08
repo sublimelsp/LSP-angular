7dfb265f7d4e1c3f641133cc9e677b7f82b10a63
Rafał Chłodnicki <rchl2k@gmail.com>
2020-11-08 19:59:36 +0100

    Use ${server_directory_path}

--- plugin.py
+++ plugin.py
@@ -17,11 +17,5 @@ class LspAngularPlugin(NpmClientHandler):
     server_binary_path = os.path.join(server_directory, "node_modules", "@angular", "language-server", "index.js")
 
     @classmethod
-    def get_binary_arguments(cls):
-        ng_log_location = os.path.join(sublime.cache_path(), cls.package_name, cls.server_directory, "ngls.log")
-        ng_probe_location = os.path.join(sublime.cache_path(), cls.package_name, cls.server_directory, "node_modules")
-        return ["--logFile", ng_log_location, "--ngProbeLocations", ng_probe_location, "--tsProbeLocations", ng_probe_location, "--stdio"]
-
-    @classmethod
     def install_in_cache(cls) -> bool:
         return False
