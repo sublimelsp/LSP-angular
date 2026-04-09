797a7493c1714a1ac9eb923363b2d390024184a5
Rafal Chlodnicki <rchl2k@gmail.com>
2026-04-09 23:18:08 +0200

    refactor: add import sorting config

--- plugin.py
+++ plugin.py
@@ -1,5 +1,7 @@
-import os
+from __future__ import annotations
+
 from lsp_utils import NpmClientHandler
+import os
 
 
 def plugin_loaded():
@@ -11,6 +13,6 @@ def plugin_unloaded():
 
 
 class LspAngularPlugin(NpmClientHandler):
-    package_name = __package__
+    package_name = str(__package__)
     server_directory = "server"
     server_binary_path = os.path.join(server_directory, "node_modules", "@angular", "language-server", "index.js")
